6d0f812b21c28694870275b233d47bdc1e9b7b1b
hexueshi <hexueshi@cnest.net>
2021-05-19 09:10:11 +0800

    Demo

--- Example/GoogleDirectionKit/KHViewController.m
+++ Example/GoogleDirectionKit/KHViewController.m
@@ -19,6 +19,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    GoogleDirectionRequestInfo *requestInfo = [[GoogleDirectionRequestInfo alloc] init];
+    requestInfo.origin = @"37.7849569, -122.4068855";
+    requestInfo.destination = @"37.7814432, -122.4460177";
+    requestInfo.apiKey = @"YOUR_API_KEY";
+    
+    [GoogleDirectionManage startAcceptDirectionInfoWith:requestInfo completion:^(BOOL success, GoogleDirectionResponseParseInfo * _Nonnull resultInfo) {
+        //
+        NSLog(@"success = %d, resultInfo = %@", success, resultInfo);
+        NSAssert(success, @"请求失败!!!");
+    }];
 }
 
 - (void)didReceiveMemoryWarning
